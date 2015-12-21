0bb5a4cc0206ea00ecbf46f7cbe2d9c99eabe7af
dabeng <zhexiany@gmail.com>
2015-12-21 19:47:56 +0800

    toggle labels

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-// Empty JS for your own code to be here
+'use strict';
+
+(function($){
+
+  function filterNutrients(event) {
+    var label = $(event.target);
+    var nutrient = label.data('nutrient');
+    var hiddenN = $('#js-nutrients').find('a').not('.' + nutrient);
+    if (!label.is('.active')) {
+      hiddenN.fadeOut();
+      label.addClass('active').siblings().removeClass('active');
+    } else {
+      hiddenN.fadeIn();
+      label.removeClass('active');
+    }
+  }
+
+  $(function() {
+    $('.label-pool').on('click', '.label', filterNutrients);
+  });
+
+})(jQuery);
